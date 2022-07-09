ac5b68e632c3bd89876cb6968348a47d3f428cf0
Daniel Haas <daniel.c.haas@gmail.com>
2022-07-09 06:17:51 -0400

    Completed the home page with background image, intro/tag and hourse of operation.

--- src/home.js
+++ src/home.js
@@ -13,6 +13,21 @@ function loadPage() {
     tag.classList.add('intro');
     tag.innerHTML=`Come taste and see the Danito's difference. Our all-you-can eat pizza buffet will excite your inner child.`;
 
+    const hrs = document.createElement('div');
+    hrs.classList.add('intro');
+    hrs.classList.add('hrs');
+    hrs.innerHTML=`
+    <span class="hr-head">Hours of Operation</span>
+    <span class="day">Monday</span> <span class="time">11:00-8:00</span>
+    <span class="day">Tuesday</span> <span class="time">11:00-8:00</span>
+    <span class="day">Wednesday</span> <span class="time">11:00-8:00</span>
+    <span class="day">Thrusday</span> <span class="time">11:00-8:00</span>
+    <span class="day">Friday</span> <span class="time">11:00-8:00</span>
+    <span class="day">Saturday</span> <span class="time">10:00-10:00</span>
+    <span class="day">Sunday</span> <span class="time">10:00-2:00</span>
+    `;
+
+
 
     const imgPizza = new Image();
     imgPizza.src = Pizza;
@@ -21,7 +36,8 @@ function loadPage() {
     // add the content
     element.appendChild(header);
     element.appendChild(tag);
-    element.appendChild(imgPizza);
+    element.appendChild(hrs);
+    // element.appendChild(imgPizza);
 
 
 
--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 import loadPage from './home.js';
 
+// define the tabs
+const tabDiv = document.createElement('div');
+
 loadPage();
 console.log('testing setup');
